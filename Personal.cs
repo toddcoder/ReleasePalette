f4fb7a9d1423e0040352ff77cdeeaf77678723eb
Todd Bennett <tebennett@eprod.com>
2021-07-14 15:10:15 -0500

    Added emails for release

---
+++ Personal.cs
@@ -0,0 +1,15 @@
+﻿namespace ReleasePalette
+{
+   public class Personal
+   {
+      public Personal()
+      {
+         OtherTo = string.Empty;
+         Signature = string.Empty;
+      }
+
+      public string OtherTo { get; set; }
+
+      public string Signature { get; set; }
+   }
+}
